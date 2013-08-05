5a7e773d7ad2275f6726f477dfe8b27465f06c78
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 14:34:28 -0500

    add users#update, users#edit, users#destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,24 @@ class UsersController < ApplicationController
     @user.save
     redirect_to @user
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+    redirect_to @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to homepage_path
+  end
 end
